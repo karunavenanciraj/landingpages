6ac95d2650758dbf42da1bdbc0eee892a1bce15f
karunavenanciraj <137624647+karunavenanciraj@users.noreply.github.com>
2024-01-09 10:07:08 +0530

    Create script.js

---
+++ instagram/script.js
@@ -0,0 +1,8 @@
+function login() {
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+
+  // Add your authentication logic here
+  // For demonstration purposes, a simple alert is used
+  alert(`Login Attempt\nUsername: ${username}\nPassword: ${password}`);
+}
