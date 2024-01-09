3499b34653a578ce9938e71a10bb3f1cdcb6b841
karunavenanciraj <137624647+karunavenanciraj@users.noreply.github.com>
2024-01-09 10:26:55 +0530

    Create script.js

---
+++ whtasapp/script.js
@@ -0,0 +1,8 @@
+function login() {
+  const phone = document.getElementById("phone").value;
+  const password = document.getElementById("password").value;
+
+  // Add your authentication logic here
+  // For demonstration purposes, a simple alert is used
+  alert(`Login Attempt\nPhone: ${phone}\nPassword: ${password}`);
+}
