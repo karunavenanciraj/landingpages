39ca516466b282f33933eb7aa19a7f4171e65e3e
karunavenanciraj <137624647+karunavenanciraj@users.noreply.github.com>
2024-01-09 10:29:31 +0530

    Create script.js

---
+++ youtube/script.js
@@ -0,0 +1,8 @@
+function login() {
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+
+  // Add your authentication logic here
+  // For demonstration purposes, a simple alert is used
+  alert(Login Attempt\nEmail: ${email}\nPassword: ${password});
+}
